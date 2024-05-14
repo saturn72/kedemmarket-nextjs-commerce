80b9dc3b36950c55211a32213eacaccec0685e10
rois <roi@saturn72.com>
2024-05-14 23:11:30 +0300

    רקצםהק רקגומגשמא בםגק

--- site/components/product/ProductCardFull/ProductCardFull.tsx
+++ site/components/product/ProductCardFull/ProductCardFull.tsx
@@ -79,34 +79,11 @@ const ProductCardFull: FC<Props> = ({
           className={s.wishlistButton}
           productId={product.id}
           variant={product.variants[0] as any}
-        />
-        {/* <IconButton
-          aria-label="Like minimal photography"
-          size="md"
-          variant="solid"
-          color="danger"
-          sx={{
-            position: 'absolute',
-            zIndex: 2,
-            borderRadius: '50%',
-            right: '1rem',
-            bottom: 0,
-            transform: 'translateY(50%)',
-          }}
-        >
-          <Favorite />
-        </IconButton> */}
-      </CardOverflow>
+        /> </CardOverflow>
 
       <CardContent style={{
         justifyContent: 'end',
       }}>
-        {/* <img
-          srcSet={`${image.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-          src={image.url}
-          alt={image.alt || product.name}
-          loading="lazy"
-        /> */}
         <Typography
           fontWeight="lg"
         >
@@ -124,82 +101,7 @@ const ProductCardFull: FC<Props> = ({
         <AddRemoveFromCart className={s.addRemoveFromCart}
           productId={product.id} active={product.active} />
       </CardContent >
-    </Card >
-    // <Card className={rootClassName} variant='outlined'>
-    //   <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-    //     <CardContent sx={{ flex: '1 0 auto' }}>
-    //       <Typography component="div" variant="h5">
-    //         {product.name}
-    //       </Typography>
-    //       {
-    //         product.description && (<Typography variant="subtitle1" color="text.secondary" component="div">
-    //           {product.description}
-    //         </Typography>)
-    //       }
-    //     </CardContent>
-    //     <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-    //       <IconButton aria-label="previous">
-    //         <Plus />
-    //       </IconButton>
-    //       <IconButton aria-label="play/pause">
-    //         <Bag sx={{ height: 38, width: 38 }} />
-    //       </IconButton>
-    //       <IconButton aria-label="next">
-    //         <Minus />
-    //       </IconButton>
-    //     </Box>
-    //   </Box>
-
-    //   <CardMedia
-    //     sx={{ maxHeight: 250, height: 140, padding: "1em 1em 0 1em", objectFit: "contain" }}
-    //     image={image.url}
-    //     title={image.alt}
-    //   />
-    //   <CardContent>
-    //     <Typography gutterBottom variant="h5" component="div">
-    //       Lizard
-    //     </Typography>
-    //     <Typography variant="body2" color="text.secondary">
-    //       Lizards are a widespread group of squamate reptiles, with over 6,000
-    //       species, ranging across all continents except Antarctica
-    //     </Typography>
-    //   </CardContent>
-    //   <CardActions>
-    //     <IconButton size="small" icon></IconButton>
-    //     <Button size="small">Remove</Button>
-    //   </CardActions>
-    // </Card >
-    // <Link
-    //   href={`/product/${product.slug}`}
-    //   className={rootClassName}
-    //   aria-label={product.name}
-    // >
-    //   <ProductTag
-    //     name={product.name}
-    //     price={`${price} ${product.price?.currencyCode}`}
-    //   />
-
-    //   <WishlistButton
-    //     className={s.wishlistButton}
-    //     productId={product.id}
-    //     variant={product.variants[0] as any}
-    //   />
-    //   {!product.active}
-
-    //   <div className={s.imageContainer}>
-    //     {product?.images && (
-    //       <Image
-    //         alt={product.name || 'Product Image'}
-    //         className={s.productImage}
-    //         src={product.images[0]?.url || placeholderImg}
-    //         height={540}
-    //         width={540}
-    //         quality="85"
-    //         {...imgProps}
-    //       />
-    //     )}
-    //   </div>
-    // </Link >
+    </Card>
   )
 }
 
